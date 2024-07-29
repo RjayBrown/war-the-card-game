1e593d2cf8eae984f9e284fbae4b6a5e01d53bde
RjayBrown <rjbrown.dev@outlook.com>
2024-07-28 21:11:09 -0400

    refactor/added war feature

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
 //Example fetch using pokemonapi.co
 document.querySelector('#newGame').addEventListener('click', getDeck)
 document.querySelector('#draw').addEventListener('click', draw)
+document.querySelector('#war').addEventListener('click', war)
 
 let cardImg1 = document.getElementById('player1')
 let cardImg2 = document.getElementById('player2')
 
-let myScoreEl = document.getElementById('myScoreEl')
-let botScoreEl = document.getElementById('botScoreEl')
+let myCountEl = document.getElementById('myCountEl')
+let botCountEl = document.getElementById('botCountEl')
+let warEl = document.getElementById('war')
 
-let myScore = Number(localStorage.getItem('myScore'))
-let botScore = Number(localStorage.getItem('botScore'))
+let myCount = Number(localStorage.getItem('myCount'))
+let botCount = Number(localStorage.getItem('botCount'))
 
-myScoreEl.innerText = myScore
-botScoreEl.innerText = botScore
+myCountEl.innerText = myCount
+botCountEl.innerText = botCount
 
 
 
 function getDeck() {
   const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 
-  myScoreEl.innerText = 0
-  botScoreEl.innerText = 0
+  myCountEl.innerText = 0
+  botCountEl.innerText = 0
+  warEl.innerText = ''
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
+      localStorage.clear()
       localStorage.setItem('deckId', data.deck_id)
       cardImg1.src = ''
       cardImg2.src = ''
-      myScore = 0
-      botScore = 0
+      myCount = 0
+      botCount = 0
 
-      localStorage.setItem('myScore', myScore)
-      localStorage.setItem('botScore', botScore)
+      localStorage.setItem('myCount', myCount)
+      localStorage.setItem('botCount', botCount)
     })
     .catch(err => {
       console.log(`error ${err}`)
@@ -43,6 +47,8 @@ function getDeck() {
 function draw() {
   let deckId = localStorage.getItem('deckId')
   const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+
+  warEl.innerText = ''
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
@@ -55,14 +61,14 @@ function draw() {
       cardImg1.src = cards[0].image
       cardImg2.src = cards[1].image
 
-      determineWinner(cards, myCard, botCard)
+      determineWinner(cards, myCard, botCard, 2)
     })
     .catch(err => {
       console.log(`error ${err}`)
     });
 }
 
-function determineWinner(cards, c1, c2) {
+function determineWinner(cards, c1, c2, cardsWon) {
   for (let card in cards) {
     card.value = 'ACE' ? card.value = 14
       : card.value = 'KING' ? card.value = 13
@@ -72,13 +78,42 @@ function determineWinner(cards, c1, c2) {
   }
 
   if (c1 > c2) {
-    myScore += 1
-    myScoreEl.innerText = `${myScore}`
+    myCount += cardsWon
   } else if (c1 < c2) {
-    botScore += 1
-    botScoreEl.innerText = `${botScore}`
+    botCount += cardsWon
+  } else {
+    warEl.innerHTML = `<button id='war'>WAR!!!</button>`
   }
 
-  localStorage.setItem('myScore', myScore)
-  localStorage.setItem('botScore', botScore)
+  myCountEl.innerText = `${myCount}`
+  botCountEl.innerText = `${botCount}`
+
+  localStorage.setItem('myCount', myCount)
+  localStorage.setItem('botCount', botCount)
+}
+
+function war() {
+  let deckId = localStorage.getItem('deckId')
+  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      let cards = data.cards
+      let myCard = cards[6].value
+      let botCard = cards[7].value
+
+      cardImg1.src = cards[6].image
+      cardImg2.src = cards[7].image
+
+      determineWinner(cards, myCard, botCard, 10)
+      warEl.innerText = ''
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+
+  localStorage.setItem('myCount', myCount)
+  localStorage.setItem('botCount', botCount)
 }
