92071ff5cd0454e2435c8834046acb0267a86ccf
RjayBrown <rjbrown.dev@outlook.com>
2024-08-21 04:54:59 -0400

    refactored to async/await

--- js/main.js
+++ js/main.js
@@ -1,285 +1,307 @@
+/* PAGE THEME */
+
+const bg = document.querySelector('html')
+const title = document.querySelector('.title')
+
+const theme = localStorage.getItem('theme')
+bg.classList.remove('reg', 'alt', 'light', 'war-bg')
+bg.classList.add(theme)
+
+function toggleMode() {
+    let theme = bg.classList.value
+    let currentTheme = ''
+    if (theme === 'reg') {
+        bg.classList.remove('reg', 'alt', 'light', 'war-bg')
+        bg.classList.add('alt')
+        currentTheme = 'alt'
+    } else if (theme === 'alt') {
+        bg.classList.remove('reg', 'alt', 'light', 'war-bg')
+        bg.classList.add('light')
+        title.classList.add('red')
+        myCountEl.classList.add('red')
+        botCountEl.classList.add('red')
+        currentTheme = 'light'
+    } else if (theme === 'light') {
+        bg.classList.remove('reg', 'alt', 'light', 'war-bg')
+        bg.classList.add('war-bg')
+        title.classList.remove('red')
+        currentTheme = 'war-bg'
+        myCountEl.classList.remove('red')
+        botCountEl.classList.remove('red')
+    } else {
+        bg.classList.remove('reg', 'alt', 'light', 'war-bg')
+        bg.classList.add('reg')
+        currentTheme = 'reg'
+    }
+    localStorage.setItem('theme', currentTheme)
+}
+
+/* PLAYER INFORMATION */
+
+const myCountEl = document.getElementById('myCountEl')
+const botCountEl = document.getElementById('botCountEl')
+
+const p1El = document.querySelector('#p1')
+const p2El = document.querySelector('#p2')
+
+const p1Card = document.getElementById('player1')
+const p2Card = document.getElementById('player2')
+
+let myCount = Number(localStorage.getItem('myCount'))
+let botCount = Number(localStorage.getItem('botCount'))
+myCountEl.innerText = myCount
+botCountEl.innerText = botCount
+
+/*
+
+Planning to add features to customize player names and choose an avatar
+
+*/
+
+/* GAME CONTROLS */
+
+const startBtn = document.querySelector('#startGame')
+const toggleBtn = document.querySelector('#toggle')
+const warEl = document.querySelector('.warEl')
+
+
+/* START GAME */
+
 document.querySelector('#startGame').addEventListener('click', getDeck)
 document.querySelector('#toggle').addEventListener('click', toggleMode)
 document.querySelector('#draw').addEventListener('click', draw)
 
-let bg = document.querySelector('html')
-let title = document.querySelector('.title')
+async function getDeck() {
+    document.querySelector('#draw').addEventListener('click', draw)
 
-let theme = localStorage.getItem('theme')
-bg.classList.remove('reg', 'alt', 'light', 'war-bg')
-bg.classList.add(`${theme}`)
+    startBtn.innerText = 'NEW GAME'
+    myCountEl.innerText = 26
+    botCountEl.innerText = 26
+    warEl.innerText = ''
 
-let cardImg1 = document.getElementById('player1')
-let cardImg2 = document.getElementById('player2')
+    try {
+        const res = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        const data = await res.json()
 
-let startBtn = document.querySelector('#startGame')
-let toggleBtn = document.querySelector('#toggle')
-let myCountEl = document.getElementById('myCountEl')
-let botCountEl = document.getElementById('botCountEl')
-let warEl = document.querySelector('.warEl')
-let p1El = document.querySelector('#p1')
-let p2El = document.querySelector('#p2')
-
-let myCount = Number(localStorage.getItem('myCount'))
-let botCount = Number(localStorage.getItem('botCount'))
-
-// for using piles
-// let myUrl
-// let botUrl
-
-myCountEl.innerText = myCount
-botCountEl.innerText = botCount
-
-/* TOGGLE LIGHT OR DARK MODE */
-
-function toggleMode() {
-  let theme = bg.classList.value
-  let currentTheme = ''
-  if (theme === 'reg') {
-    bg.classList.remove('reg')
-    bg.classList.add('alt')
-    currentTheme = 'alt'
-  } else if (theme === 'alt') {
-    bg.classList.remove('alt')
-    bg.classList.add('light')
-    title.classList.add('red')
-    myCountEl.classList.add('red')
-    botCountEl.classList.add('red')
-    currentTheme = 'light'
-  } else if (theme === 'light') {
-    bg.classList.remove('light')
-    bg.classList.add('war-bg')
-    title.classList.remove('red')
-    currentTheme = 'war-bg'
-    myCountEl.classList.remove('red')
-    botCountEl.classList.remove('red')
-  } else {
-    bg.classList.remove('war-bg')
-    bg.classList.add('reg')
-    currentTheme = 'reg'
-  }
-  localStorage.setItem('theme', currentTheme)
-}
-
-/* START GAME */
-
-function getDeck() {
-  document.querySelector('#draw').addEventListener('click', draw)
-
-  const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-
-  startBtn.innerText = 'NEW GAME'
-  myCountEl.innerText = 26
-  botCountEl.innerText = 26
-  warEl.innerText = ''
-
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-      localStorage.clear()
-      localStorage.setItem('deckId', data.deck_id)
-      cardImg1.src = ''
-      cardImg2.src = ''
-      myCount = 26
-      botCount = 26
-      p1El.innerText = ''
-      p2El.innerText = ''
-
-      localStorage.setItem('myCount', myCount)
-      localStorage.setItem('botCount', botCount)
-
-
-      /* USING PILES TO KEEP TRACK OF CARDS  */
-
-      // let deckId = localStorage.getItem('deckId')
-
-      // myUrl = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/mypile/add/?cards=`
-      // botUrl = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/botpile/add/?cards=`
-      // fetch(myUrl)
-      //   .then(res => res.json()) // parse response as JSON
-      //   .then(data => {
-      //     console.log(data)
-      //   })
-      //   .catch(err => {
-      //     console.log(`error ${err}`)
-      //   });
-      // fetch(botUrl)
-      //   .then(res => res.json()) // parse response as JSON
-      //   .then(data => {
-      //     console.log(data)
-      //   })
-      //   .catch(err => {
-      //     console.log(`error ${err}`)
-      //   });
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    })
-}
-
-/* DRAW */
-
-function draw() {
-  // Draw function must change if using piles
-  let deckId = localStorage.getItem('deckId')
-  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
-  const shuffle = `https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`
-
-  warEl.innerText = ''
-
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      if (!data.error) {
         console.log(data)
-        let cards = data.cards
-        let myCard = cards[0]
-        let botCard = cards[1]
+        localStorage.clear()
+        localStorage.setItem('deckId', data.deck_id)
+        p1Card.src = ''
+        p2Card.src = ''
+        myCount = 26
+        botCount = 26
+        p1El.innerText = ''
+        p2El.innerText = ''
 
-        cardImg1.src = cards[0].image
-        cardImg2.src = cards[1].image
+        localStorage.setItem('myCount', myCount)
+        localStorage.setItem('botCount', botCount)
+    }
 
-        determineWinner(cards, myCard, botCard, 2)
-      } else {
-        fetch(shuffle)
-          .then(res => res.json()) // parse response as JSON
-          .then(data => {
-            console.log(data)
-          })
-          .catch(err => {
-            console.log(`error ${err}`)
-          });
-        fetch(url)
-          .then(res => res.json()) // parse response as JSON
-          .then(data => {
+    catch {
+        const err = `Error: Failed to retreive new deck`
+        console.log(err)
+    }
+
+    /* USING PILES TO KEEP TRACK OF CARDS  */
+
+    /*
+    const deckId = localStorage.getItem('deckId')
+
+    myDrawPile = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/myDrawPile/add/?cards=`
+    mySidePile = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/mySidePile/add/?cards=`
+    botDrawPile = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/botDrawPile/add/?cards=`
+    botSidePile = `https://www.deckofcardsapi.com/api/deck/${deckId}/pile/botSidePile/add/?cards=`
+    */
+
+
+}
+
+/* DRAWING CARDS */
+
+async function draw() {
+    /*
+
+    Functinality to be added for tracking player cards. Each player will have two piles, one to draw from, and one to store the cards won. When the draw pile is empty, cards won will be shuffled and moved to the draw pile.
+
+    A potential twist will be added to allow for cards to be 'raided' when certain conditions are met
+
+    */
+    const deckId = localStorage.getItem('deckId')
+    const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    const shuffle = `https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`
+
+    warEl.innerText = ''
+
+    try {
+        const drawRes = await fetch(url)
+        const data = await drawRes.json()
+
+        if (!data.error) {
             console.log(data)
             let cards = data.cards
             let myCard = cards[0]
             let botCard = cards[1]
 
-            cardImg1.src = cards[0].image
-            cardImg2.src = cards[1].image
+            p1Card.src = cards[0].image
+            p2Card.src = cards[1].image
 
             determineWinner(cards, myCard, botCard, 2)
-          })
-          .catch(err => {
-            console.log(`error ${err}`)
-          });
-      }
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
+        } else {
+            try {
+                const shuffleRes = await fetch(shuffle)
+                const data = await shuffleRes.json()
+                console.log('Deck Shuffled', data)
+            }
+            catch {
+                const err = `Error: Failed to shuffle cards`
+                console.log(err)
+            }
+            try {
+                const drawRes = await fetch(url)
+                const data = await drawRes.json()
+
+                if (!data.error) {
+                    console.log(data)
+                    let cards = data.cards
+                    let myCard = cards[0]
+                    let botCard = cards[1]
+
+                    p1Card.src = cards[0].image
+                    p2Card.src = cards[1].image
+
+                    determineWinner(cards, myCard, botCard, 2)
+                }
+            }
+            catch {
+                const err = `Error: Failed to draw cards`
+                console.log(err)
+            }
+        }
+    }
+    catch {
+        const err = `Error: Failed to draw cards`
+        console.log(err)
+    }
 }
 
 function determineWinner(cards, c1, c2, cardsWon) {
-  // figure out how to add war count - # to multiply draw cards in war function API call
+    // figure out how to add war count - # to multiply draw cards in war function API call
 
-  for (let card in cards) {
-    card.value = 'ACE' ? card.value = 14
-      : card.value = 'KING' ? card.value = 13
-        : card.value = 'QUEEN' ? card.value = 12
-          : card.value = 'JACK' ? card.value = 11
-            : card.value = Number(card.value)
-  }
-
-  if (c1.value > c2.value) {
-    myCount += cardsWon
-    botCount -= cardsWon
-    document.querySelector('#draw').addEventListener('click', draw)
-    if (+botCount <= 0) {
-      botCount = 0
-      myCount = 52
-      botCountEl.innerText = `${botCount}`
-      endGame()
+    for (let card in cards) {
+        card.value = 'ACE' ? card.value = 14
+            : card.value = 'KING' ? card.value = 13
+                : card.value = 'QUEEN' ? card.value = 12
+                    : card.value = 'JACK' ? card.value = 11
+                        : card.value = Number(card.value)
     }
-  } else if (c1.value < c2.value) {
-    botCount += cardsWon
-    myCount -= cardsWon
-    document.querySelector('#draw').addEventListener('click', draw)
-    if (+myCount <= 0) {
-      myCount = 0
-      botCount = 52
-      myCountEl.innerText = `${myCount}`
-      endGame()
+
+    if (c1.value > c2.value) {
+        myCount += cardsWon
+        botCount -= cardsWon
+        document.querySelector('#draw').addEventListener('click', draw)
+        if (+botCount <= 0) {
+            botCount = 0
+            myCount = 52
+            botCountEl.innerText = `${botCount}`
+            endGame()
+        }
+    } else if (c1.value < c2.value) {
+        botCount += cardsWon
+        myCount -= cardsWon
+        document.querySelector('#draw').addEventListener('click', draw)
+        if (+myCount <= 0) {
+            myCount = 0
+            botCount = 52
+            myCountEl.innerText = `${myCount}`
+            endGame()
+        }
+    } else {
+        bg.classList.add('war')
+        title.classList.remove('red')
+        myCountEl.classList.remove('red')
+        botCountEl.classList.remove('red')
+        warEl.innerHTML = `<button id='war'>WAR!</button>`
+        document.querySelector('#draw').removeEventListener('click', draw)
+        document.querySelector('#war').addEventListener('click', war)
     }
-  } else {
-    bg.classList.add('war')
-    title.classList.remove('red')
-    myCountEl.classList.remove('red')
-    botCountEl.classList.remove('red')
-    warEl.innerHTML = `<button id='war'>WAR!</button>`
-    document.querySelector('#draw').removeEventListener('click', draw)
-    document.querySelector('#war').addEventListener('click', war)
-  }
 
-  myCountEl.innerText = `${myCount}`
-  botCountEl.innerText = `${botCount}`
+    myCountEl.innerText = `${myCount}`
+    botCountEl.innerText = `${botCount}`
 
-  localStorage.setItem('myCount', myCount)
-  localStorage.setItem('botCount', botCount)
+    localStorage.setItem('myCount', myCount)
+    localStorage.setItem('botCount', botCount)
 }
 
-function war() {
-  let deckId = localStorage.getItem('deckId')
-  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`
-  const shuffle = `https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`
+async function war() {
+    /*
 
-  bg.classList.remove('war')
-  if (theme === 'light') {
-    title.classList.add('red')
-    myCountEl.classList.add('red')
-    botCountEl.classList.add('red')
-  }
+    Game currently crashes and needs to be reset when there isn't enough cards in the deck to draw for war, should be fixed when the functionality for tracking cards with piles is added
 
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-      if (data.remaining === 0) {
-        fetch(shuffle)
-          .then(res => res.json()) // parse response as JSON
-          .then(data => {
-            console.log(data)
-            warEl.innerHTML = `<button id="war" type="button" name="button">WAR!</button>`
-            document.querySelector('#draw').removeEventListener('click', draw)
-            document.querySelector('#war').addEventListener('click', war)
-          })
-          .catch(err => {
-            console.log(`error ${err}`)
-          });
-      }
-      let cards = data.cards
-      let myCard = cards[6]
-      let botCard = cards[7]
+    */
+    let deckId = localStorage.getItem('deckId')
+    const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`
+    const shuffle = `https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`
 
-      cardImg1.src = cards[6].image
-      cardImg2.src = cards[7].image
+    localStorage.getItem('theme')
+    bg.classList.remove('war')
+    if (theme === 'light') {
+        title.classList.add('red')
+        myCountEl.classList.add('red')
+        botCountEl.classList.add('red')
+    }
 
-      determineWinner(cards, myCard, botCard, 10)
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
+    try {
+        const warRes = await fetch(url)
+        const data = await warRes.json()
+        console.log(data)
 
+        if (data.remaining === 0) {
+            try {
+                const warShuffleRes = await fetch(shuffle)
+                const data = warShuffleRes.json()
+                console.log(data)
 
-  warEl.innerText = ''
+                warEl.innerHTML = `<button id="war" type="button" name="button">WAR!</button>`
+                document.querySelector('#draw').removeEventListener('click', draw)
+                document.querySelector('#war').addEventListener('click', war)
+            }
+            catch {
+                const err = `Error: Failed to shuffle cards`
+                console.log(err)
+            }
+        }
+        let cards = data.cards
+        let myCard = cards[6]
+        let botCard = cards[7]
 
-  localStorage.setItem('myCount', myCount)
-  localStorage.setItem('botCount', botCount)
+        p1Card.src = cards[6].image
+        p2Card.src = cards[7].image
+
+        determineWinner(cards, myCard, botCard, 10)
+    }
+
+    catch {
+        const err = `Error: Failed to draw war cards`
+        console.log(err)
+    }
+
+    warEl.innerText = ''
+
+    localStorage.setItem('myCount', myCount)
+    localStorage.setItem('botCount', botCount)
 }
 
 function endGame() {
-  if (+myCount === 0) {
-    p2El.classList.add('winner')
-    p2El.innerText = 'BOT WINS!'
-    startBtn.innerText = 'REMATCH'
-    document.querySelector('#draw').removeEventListener('click', draw)
-  } else if (+botCount === 0) {
-    p1El.classList.add('winner')
-    p1El.innerText = 'YOU WIN!'
-    startBtn.innerText = 'REMATCH'
-    document.querySelector('#draw').removeEventListener('click', draw)
-  }
+    if (+myCount === 0) {
+        p2El.classList.add('winner')
+        p2El.innerText = 'BOT WINS!'
+        startBtn.innerText = 'REMATCH'
+        document.querySelector('#draw').removeEventListener('click', draw)
+    } else if (+botCount === 0) {
+        p1El.classList.add('winner')
+        p1El.innerText = 'YOU WIN!'
+        startBtn.innerText = 'REMATCH'
+        document.querySelector('#draw').removeEventListener('click', draw)
+    }
 }
+
